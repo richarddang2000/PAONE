d4827c02e260360484687125b401298327a6a83d
richarddang2000 <63926205+richarddang2000@users.noreply.github.com>
2020-09-06 19:53:14 -0500

    Add files via upload

--- client.cpp
+++ client.cpp
@@ -7,34 +7,32 @@
 #include "common.h"
 #include "FIFOreqchannel.h"
 #include<sys/wait.h>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    //fork and execvp
+
+    //FORK and EXECVP
     char* args [] = {"./server", "p:t:e:f:m:"};
     pid_t pid = fork();
     cout << "pid = " << pid << endl;
     if (pid == 0){
-        wait(NULL);
         execvp(args[0], args);//run server
     }
-
 
     int person;
     float seconds;
     int ecgno;
     string fname;
+    int buffer_capacity;
     bool file_transfer = false; //Determines whether to to get datamessage or file message
+    bool changeBuffer = false; //indicates whether original buffer capacity was changed
+
+
     FIFORequestChannel chan("control", FIFORequestChannel::CLIENT_SIDE);
 
-    //NEW CHANNEL
     //FIFORequestChannel chan2 ("control2", FIFORequestChannel::CLIENT_SIDE);
-
-    // sending a non-sense message, you need to change this
-    //char buf [MAX_MESSAGE];
-    // datamsg* x = new datamsg (1, 0.004, 2);
-
     int opt;
     while ((opt = getopt(argc, argv, "p:t:e:f:m:")) != -1) {
         switch (opt) {
@@ -52,9 +50,13 @@ int main(int argc, char *argv[]) {
                 file_transfer = true;
                 break;
             case 'm':
+                buffer_capacity = atoi(optarg);
+                changeBuffer = true;
                 break;
         }
     }
+
+
     //if data message chosen, execute loop below
     if (!file_transfer) {
         datamsg *x = new datamsg(person, seconds, ecgno);
@@ -70,6 +72,18 @@ int main(int argc, char *argv[]) {
         delete reply;
     }
 	//FILE Transfer *************************************************************
+	//change length of buffer depending on whether -m was called or not
+	int buffer_length;
+
+    if (changeBuffer == true) {
+        buffer_length = buffer_capacity;
+
+    }
+    else {
+        buffer_length = MAX_MESSAGE;
+    }
+
+    cout << "buffer_length: " << buffer_length << endl;
 	if (file_transfer) {
         string fpath = "received/" + fname ; //file to write to
 	    ofstream myfile;
@@ -79,7 +93,7 @@ int main(int argc, char *argv[]) {
 
 
         filemsg fm(_offset, _len);
-        char *buf = new char[MAX_MESSAGE];
+        char *buf = new char[buffer_length];
         //char fname[] = "teslkansdlkjflasjdf.dat";
         memcpy(buf, &fm, sizeof(filemsg));
         strcpy(buf + sizeof(filemsg), fname.c_str());
@@ -97,13 +111,13 @@ int main(int argc, char *argv[]) {
         //int temp = 1000; //to be replaced with file length later
         while (file_length > 0) {
 
-            if (file_length >= MAX_MESSAGE) {
-                _len = MAX_MESSAGE;
+            if (file_length >= buffer_length) {
+                _len = buffer_length;
             } else {
                 _len = file_length;
             }
             filemsg newFM(index, _len);
-            char *newBuf = new char[MAX_MESSAGE];
+            char *newBuf = new char[buffer_length];
             //char fname[] = "teslkansdlkjflasjdf.dat";
             memcpy(newBuf, &newFM, sizeof(filemsg));
             strcpy(newBuf + sizeof(filemsg), fname.c_str());
@@ -129,6 +143,5 @@ int main(int argc, char *argv[]) {
     chan.cwrite (&m, sizeof (MESSAGE_TYPE));
     //closing the new channel
     //chan2.cwrite (&m, sizeof (MESSAGE_TYPE));
-
 
 }
